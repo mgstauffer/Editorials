b147088aae2abe2c95563bf0b8670d4a75c0ec94
Michael Stauffer <mgstauff@gmail.com>
2022-02-14 00:39:30 -0500

    Changed Obj Hotkey Selector
    - it automatically saves changes now
    - it properly updates when window is shown and after compiling

--- Editor/ObjectSelectorWindow.cs
+++ Editor/ObjectSelectorWindow.cs
@@ -7,13 +7,50 @@ using System.IO;
 public class ObjectSelectorWindow : EditorWindow
 {
     GameObject g1, g2, g3, g4, g5;
-    
+
+    static private string SettingsFilePath
+    {
+        get {
+            string directory = ("Assets/Editorials/SelectData");
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+            return directory + "/" + "ObjSelect" + ".select";
+        }
+    }
+
     [MenuItem("Tools/Editorials/Object HotKey Selector Window")]
+    //NOTE - only gets called when window is opened, not when it's
+    // already in the editor as a panel and just revealed
     public static void ShowWindow()
     {
         EditorWindow win = EditorWindow.GetWindow<ObjectSelectorWindow>("Object HotKey Selector Window");
         win.maxSize = new Vector2(300, 110);
         win.maxSize = win.maxSize;
+    }
+ 
+    //This automatically checks for changes and merges with existing 
+    // saved settings and then saves again. No need for a save button anymore
+    //This is only reliable why I found to update things whenever needed.
+    //There are callbacks like OnFocus that help, but couldn't find anything
+    // to get a callback when a value is changed in the editor window, or 
+    // when editor returns from compiling.
+    //Called 10 times per second.
+    private void OnInspectorUpdate()
+    {
+        //This merges any unsaved changes from the fields with
+        // what's already been saved, then saves it all. Nice.
+        if (!EditorApplication.isPlaying)
+        {
+            string[] objs = GetSavedSelections().objs;
+            g1 = g1 != null ? g1 : GameObject.Find(objs[0]); //returns null if not found
+            g2 = g2 != null ? g2 : GameObject.Find(objs[1]);
+            g3 = g3 != null ? g3 : GameObject.Find(objs[2]);
+            g4 = g4 != null ? g4 : GameObject.Find(objs[3]);
+            g5 = g5 != null ? g5 : GameObject.Find(objs[4]);
+            Save();
+        }
     }
 
     void OnGUI()
@@ -51,19 +88,18 @@ public class ObjectSelectorWindow : EditorWindow
         g5 = (GameObject)obj5;
         EditorGUILayout.EndHorizontal();
 
+        /* we auto-save now
         if (GUILayout.Button("Save"))
         {
             Save();
         }
-
+        */
     }
 
     public void Save()
     {
         SelectionData dat = new SelectionData();
-        
-
-        
+                      
         dat.objs[0] = g1 != null ? g1.name : "empty";   
         dat.objs[1] = g2 != null ? g2.name : "empty";
         dat.objs[2] = g3 != null ? g3.name : "empty";
@@ -71,31 +107,22 @@ public class ObjectSelectorWindow : EditorWindow
         dat.objs[4] = g5 != null ? g5.name : "empty";
 
         string dataAsJson = JsonUtility.ToJson(dat, true);
-        string directory = ("Assets/Editorials/SelectData");
-
-        string FilePath = directory + "/" + "ObjSelect" + ".select";
-        DirectoryInfo dir = new DirectoryInfo(FilePath);
-        if (!Directory.Exists(directory))
+        DirectoryInfo dir = new DirectoryInfo(SettingsFilePath);
+        if (File.Exists(SettingsFilePath))
         {
-            Directory.CreateDirectory(directory);
-        }
-        if (File.Exists(FilePath))
-        {
-            File.Delete(FilePath);
-        }
-        
-        
+            File.Delete(SettingsFilePath);
+        }        
 
-        File.WriteAllText(FilePath, dataAsJson);
+        File.WriteAllText(SettingsFilePath, dataAsJson);
 
     }
 
     [MenuItem("Tools/Editorials/ObjSelect/1 &#1")]
     public static void Select1()
     {
-        if (Select().objs[0] != "empty")
+        if (GetSavedSelections().objs[0] != "empty")
         {
-            Selection.activeGameObject = GameObject.Find(Select().objs[0]);
+            Selection.activeGameObject = GameObject.Find(GetSavedSelections().objs[0]);
         }
         else
         {
@@ -106,9 +133,9 @@ public class ObjectSelectorWindow : EditorWindow
     [MenuItem("Tools/Editorials/ObjSelect/2 &#2")]
     public static void Select2()
     {
-        if (Select().objs[1] != "empty")
+        if (GetSavedSelections().objs[1] != "empty")
         {
-            Selection.activeGameObject = GameObject.Find(Select().objs[1]);
+            Selection.activeGameObject = GameObject.Find(GetSavedSelections().objs[1]);
         }
         else
         {
@@ -118,9 +145,9 @@ public class ObjectSelectorWindow : EditorWindow
     [MenuItem("Tools/Editorials/ObjSelect/3 &#3")]
     public static void Select3()
     {
-        if (Select().objs[2] != "empty")
+        if (GetSavedSelections().objs[2] != "empty")
         {
-            Selection.activeGameObject = GameObject.Find(Select().objs[2]);
+            Selection.activeGameObject = GameObject.Find(GetSavedSelections().objs[2]);
         }
         else
         {
@@ -131,9 +158,9 @@ public class ObjectSelectorWindow : EditorWindow
     [MenuItem("Tools/Editorials/ObjSelect/4 &#4")]
     public static void Select4()
     {
-        if (Select().objs[3] != "empty")
+        if (GetSavedSelections().objs[3] != "empty")
         {
-            Selection.activeGameObject = GameObject.Find(Select().objs[3]);
+            Selection.activeGameObject = GameObject.Find(GetSavedSelections().objs[3]);
         }
         else
         {
@@ -144,9 +171,9 @@ public class ObjectSelectorWindow : EditorWindow
     [MenuItem("Tools/Editorials/ObjSelect/5 &#5")]
     public static void Select5()
     {
-        if (Select().objs[4] != "empty")
+        if (GetSavedSelections().objs[4] != "empty")
         {
-            Selection.activeGameObject = GameObject.Find(Select().objs[4]);
+            Selection.activeGameObject = GameObject.Find(GetSavedSelections().objs[4]);
         }
         else
         {
@@ -154,9 +181,9 @@ public class ObjectSelectorWindow : EditorWindow
         }
     }
 
-    static SelectionData Select()
+    static SelectionData GetSavedSelections()
     {
-        string loadedLevel = File.ReadAllText("Assets/Editorials/SelectData/ObjSelect.select");
+        string loadedLevel = File.ReadAllText(SettingsFilePath);
         SelectionData data = JsonUtility.FromJson<SelectionData>(loadedLevel);
         
         return data;
